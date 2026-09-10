95a22d8d48402883cc9f99d059c44754dc252fb6
mahshad <mahshadayari@gmail.com>
2026-09-10 12:38:51 +0300

    improve codes for adding dots

--- js/script.js
+++ js/script.js
@@ -5,29 +5,71 @@ const images = [
   "images/4.jpg",
   "images/5.jpeg",
 ];
+
 let currentIndex = 0;
+
 const sliderImage = document.querySelector("#sliderImage");
+
 const prevBtn = document.querySelector("#prevBtn");
+
 const nextBtn = document.querySelector("#nextBtn");
+
+const sliderDots = document.querySelector("#sliderDots");
 
 function showImage() {
   sliderImage.src = images[currentIndex];
+
+  const dots = document.querySelectorAll(".dot");
+
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("active", index === currentIndex);
+  });
 }
+
+function createDots() {
+  images.forEach((image, index) => {
+    const dot = document.createElement("button");
+
+    dot.classList.add("dot");
+
+    if (index === currentIndex) {
+      dot.classList.add("active");
+    }
+
+    dot.addEventListener("click", () => {
+      currentIndex = index;
+
+      showImage();
+    });
+
+    sliderDots.appendChild(dot);
+  });
+}
+
 nextBtn.addEventListener("click", () => {
   currentIndex++;
+
   if (currentIndex >= images.length) {
     currentIndex = 0;
   }
+
   showImage();
 });
+
 prevBtn.addEventListener("click", () => {
   currentIndex--;
+
   if (currentIndex < 0) {
     currentIndex = images.length - 1;
   }
+
   showImage();
 });
+
+createDots();
+
 showImage();
+
 setInterval(() => {
   currentIndex++;
 
